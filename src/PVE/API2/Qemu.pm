hwinther/qemu-server

commit 1d335f866062a806bbb7f18f148e1e15583553cd

api: vncproxy: use generate_vnc_password() function from ticket module
The $gen_rand_chars helper was renamed and moved to the PVE::Ticket
module, since it is also required in pve-manager and pve-container.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/API2/Qemu.pm
@@ -66,6 +66,7 @@ use PVE::Replication;
 use PVE::ReplicationState;
 use PVE::StorageTunnel;
 use PVE::RESTEnvironment qw(log_warn);
+use PVE::Ticket;
 
 BEGIN {
     if (!$ENV{PVE_GENERATING_DOCS}) {
@@ -2877,22 +2878,6 @@ __PACKAGE__->register_method({
 });
 
 # uses good entropy, each char is limited to 6 bit to get printable chars simply
-my $gen_rand_chars = sub {
-    my ($length) = @_;
-
-    die "invalid length $length" if $length < 1;
-
-    my $min = ord('!'); # first printable ascii
-
-    my $rand_bytes = Crypt::OpenSSL::Random::random_bytes($length);
-    die "failed to generate random bytes!\n"
-        if !$rand_bytes;
-
-    my $str = join('', map { chr((ord($_) & 0x3F) + $min) } split('', $rand_bytes));
-
-    return $str;
-};
-
 my $sslcert;
 
 __PACKAGE__->register_method({
@@ -2982,7 +2967,7 @@ __PACKAGE__->register_method({
         my $ticket = PVE::AccessControl::assemble_vnc_ticket($authuser, $authpath, $port);
         my $password = $ticket;
         if ($param->{'generate-password'}) {
-            $password = $gen_rand_chars->(8);
+            $password = PVE::Ticket::generate_vnc_password(8);
         }
 
         my $timeout = 10;

commit eaef6d6452e9e225ffc36a7af9b0427816909d2a

api: vncproxy: drop superfluous parameter from generate_vnc_password() call
The definition of generate_vnc_password() in PVE::Ticket does not take
a parameter for the length, since the password is fixed to 8 bytes in
the VNC protocol.

Fixes: 1d335f86 ("api: vncproxy: use generate_vnc_password() function from ticket module")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/API2/Qemu.pm
@@ -2969,7 +2969,7 @@ __PACKAGE__->register_method({
         my $ticket = PVE::AccessControl::assemble_vnc_ticket($authuser, $authpath, $port);
         my $password;
         if ($param->{'generate-password'} || !defined($serial) || $param->{websocket}) {
-            $password = PVE::Ticket::generate_vnc_password(8);
+            $password = PVE::Ticket::generate_vnc_password();
             # FIXME: MAJOR VERSION: Avoid this hack, require using explicit 'password' return value
             $ticket = "${password}:${ticket}";
         } # else authentication happens via ticket only, not via password in VNC protocol