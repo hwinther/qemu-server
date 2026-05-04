hwinther/qemu-server

commit 7127e2ae6533f0eca0eb63b3825f74554e269e49

api: dump cloud-init: mask password for user without VM.Config.Cloudinit
The endpoint for dumping the cloud-init configuration only requires
'VM.Audit' permissions. However, the password is sensitive and should
only be shown to users with 'VM.Config.Cloudinit' privileges. Such
users can also modify the password.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Reviewed-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/API2/Qemu.pm
@@ -6535,10 +6535,20 @@ __PACKAGE__->register_method({
     code => sub {
         my ($param) = @_;
 
+        my $rpcenv = PVE::RPCEnvironment::get();
+        my $authuser = $rpcenv->get_user();
+
         my $conf = PVE::QemuConfig->load_config($param->{vmid});
 
-        return PVE::QemuServer::Cloudinit::dump_cloudinit_config($conf, $param->{vmid},
-            $param->{type});
+        my $mask_password =
+            !$rpcenv->check($authuser, '/vms/{vmid}', ['VM.Config.Cloudinit'], 1);
+
+        return PVE::QemuServer::Cloudinit::dump_cloudinit_config(
+            $conf,
+            $param->{vmid},
+            $param->{type},
+            $mask_password,
+        );
     },
 });
 

src/PVE/QemuServer/Cloudinit.pm
@@ -125,7 +125,7 @@ sub get_dns_conf {
 }
 
 sub cloudinit_userdata {
-    my ($conf, $vmid) = @_;
+    my ($conf, $vmid, $mask_password) = @_;
 
     my ($hostname, $fqdn) = get_hostname_fqdn($conf, $vmid);
 
@@ -140,7 +140,13 @@ sub cloudinit_userdata {
 
     $content .= "user: $username\n" if defined($username);
     $content .= "disable_root: False\n" if defined($username) && $username eq 'root';
-    $content .= "password: $password\n" if defined($password);
+    if (defined($password)) {
+        if ($mask_password) {
+            $content .= "password: **********\n";
+        } else {
+            $content .= "password: $password\n";
+        }
+    }
 
     if (defined(my $keys = $conf->{sshkeys})) {
         $keys = URI::Escape::uri_unescape($keys);
@@ -715,12 +721,12 @@ sub apply_cloudinit_config {
 }
 
 sub dump_cloudinit_config {
-    my ($conf, $vmid, $type) = @_;
+    my ($conf, $vmid, $type, $mask_password) = @_;
 
     my $format = get_cloudinit_format($conf);
 
     if ($type eq 'user') {
-        return cloudinit_userdata($conf, $vmid);
+        return cloudinit_userdata($conf, $vmid, $mask_password);
     } elsif ($type eq 'network') {
         if ($format eq 'nocloud') {
             return nocloud_network($conf);
@@ -728,6 +734,7 @@ sub dump_cloudinit_config {
             return configdrive2_network($conf);
         }
     } else { # metadata config
+        # Don't mask password here to get correct uuid.
         my $user = cloudinit_userdata($conf, $vmid);
         if ($format eq 'nocloud') {
             my $network = nocloud_network($conf);

commit 0b36a0bae1e84eb11122a7483bc2b0a615f94a56

api: cloudinit: interpolate vmid in dump endpoint's permission check
VM.Config.Cloudinit holders never got the unmasked output their grant
entitles them to.

The mask-password decision compared the caller's privileges against
the literal '/vms/{vmid}' string rather than the request's actual
VMID, as we only do such param interpolation for permissions encoded
in the API rest handler, as those are interpreted differently (see
PVE::RPCEnvironment::exec_api2_perm_check).

Manually checking permissions only short-circuits root@pam to
Administrator on any path so root was unaffected, every non-root
caller hit compile_acl_path against the literal placeholder which
never matches a real ACL entry, so the check returned false
unconditionally Fix the interpolation so non-root delegates see what
their grant entitles them to.

Fixes: 7127e2ae ("api: dump cloud-init: mask password for user without VM.Config.Cloudinit")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/API2/Qemu.pm
@@ -6546,7 +6546,7 @@ __PACKAGE__->register_method({
         my $conf = PVE::QemuConfig->load_config($param->{vmid});
 
         my $mask_password =
-            !$rpcenv->check($authuser, '/vms/{vmid}', ['VM.Config.Cloudinit'], 1);
+            !$rpcenv->check($authuser, "/vms/$param->{vmid}", ['VM.Config.Cloudinit'], 1);
 
         return PVE::QemuServer::Cloudinit::dump_cloudinit_config(
             $conf,